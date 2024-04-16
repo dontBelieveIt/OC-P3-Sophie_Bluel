a00314191efe29d7575100abade9fd350cdaaca2
Roxane <roxane.vidoni@gmail.com>
2024-04-16 16:50:01 +0200

    still in the modifiction of the login script

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -2,7 +2,7 @@
 const loginForm = document.getElementById("loginBox");
 const email = document.getElementById("loginMail");
 const password = document.getElementById("loginPassword");
-
+const apiUrl = "http://localhost:5678/api"
 
 
 function validerMail(email) {
@@ -13,34 +13,38 @@ function validerMail(email) {
     return false
 }
 
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    validerMail();
-    
-    const email = document.getElementById("loginMail"); 
-    const password =document.getElementById("loginPassword"); 
-    const loginData = new FormData(loginForm);
-    const formData = JSON.stringify(login);
-        fetch('http://localhost:5678/api/', {
-        method: 'POST',
-        body: loginData
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Handle successful login response
-        // For example, you can redirect the user to another page
-        window.location.href = 'homepageLoggedIn.html';
-    })
-    .catch(error => {
-        // Handle errors, e.g., incorrect credentials
-        // document.getElementById('message').innerText = 'Incorrect username or password';
-        console.error('There was a problem with the fetch operation:', error);
-    });
-    
-});
+(() =>
+     {
+        loginForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            validerMail();
+            
+            const email = document.getElementById("loginMail"); 
+            const password =document.getElementById("loginPassword"); 
+            const loginData = JSON.stringify({"email":email.value, "password":password.value});
+            fetch(apiUrl + '/users/login', {
+                method: 'POST',
+                body: loginData
+            })
+            
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Handle successful login response
+                // For example, you can redirect the user to another page
+                window.location.href = 'homepageLoggedIn.html';
+            })
+            .catch(error => {
+                // Handle errors, e.g., incorrect credentials
+                // document.getElementById('message').innerText = 'Incorrect username or password';
+                console.error('There was a problem with the fetch operation:', error);
+            });
+            
+        });
+     }
+)();
 
