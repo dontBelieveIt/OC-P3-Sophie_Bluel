9b478c2f9507dd5fe299709325a7dee53e174999
Roxane <roxane.vidoni@gmail.com>
2024-04-16 16:10:59 +0200

    First try on the logging JS in Script.js

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,11 +1,46 @@
-const openModal = document.querySelector("[data-open-modal]"),
-      modal = document.querySelector("[data-modal]"),
-      closeModal = document.querySelector("[data-close-modal]");
+// login script
+const loginForm = document.getElementById("loginBox");
+const email = document.getElementById("loginMail");
+const password = document.getElementById("loginPassword");
 
-openModal.addEventListener("click", () => {
-  modal.showModal();
+
+
+function validerMail(email) {
+    let mailRegEx = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[[a-z0-9._-]+]")
+    if (mailRegEx.test(email)) {
+        return true
+    }
+    return false
+}
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validerMail();
+    
+    const email = document.getElementById("loginMail"); 
+    const password =document.getElementById("loginPassword"); 
+    const loginData = new FormData(loginForm);
+    const formData = JSON.stringify(login);
+        fetch('http://localhost:5678/api/', {
+        method: 'POST',
+        body: loginData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Handle successful login response
+        // For example, you can redirect the user to another page
+        window.location.href = 'homepageLoggedIn.html';
+    })
+    .catch(error => {
+        // Handle errors, e.g., incorrect credentials
+        // document.getElementById('message').innerText = 'Incorrect username or password';
+        console.error('There was a problem with the fetch operation:', error);
+    });
+    
 });
 
-closeModal.addEventListener("click", () => {
-  modal.close();
-});
