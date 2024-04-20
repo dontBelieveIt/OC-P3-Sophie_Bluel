eb66d24a131ff147ef136c222b9c8da5b556d5af
Roxane <roxane.vidoni@gmail.com>
2024-04-20 19:46:27 +0200

    Filters done for the index.html

--- FrontEnd/assets/filter.js
+++ FrontEnd/assets/filter.js
@@ -1,10 +1,11 @@
-// Get for works
+// Get for works from backend
 const response = await fetch('http://localhost:5678/api/works');
 let works = await response.json(); 
-// Get for categories (filtered Work)
+// Get for categories (filtered Work) from backend
 const resp = await fetch('http://localhost:5678/api/categories');
 let category = await resp.json();
 
+/**********************************************generer photo galerie**********************************************************/
 const photoZone = document.querySelector(".gallery");
 function genererPhotos(works){
     for (let i = 0; i < works.length; i++) {
@@ -26,28 +27,13 @@ function genererPhotos(works){
 }
 genererPhotos(works);
 
-/**********************************************All Functions**********************************************************/
+/********************************************************************************************************************************
+ ********************************FILTERED FUNCTION AND EVENTS LISTENER***********************************************************
+ ********************************************************************************************************************************/
 function noFilterBtnClass(noFilterBtn) {
     noFilterBtn.classList.add("filter-btn"); 
     noFilterBtn.classList.remove("active");
 };
-
-function filterThis(filteredWork) {
-    photoZone.innerHTML = "";
-    for (let i = 0; i < works.length; i++) {
-        const workPhoto = works[i];
-        const workCategory = workPhoto.category; 
-        const categoryName = workCategory.name; 
-
-        if (workCategory === categoryId) {
-            return true
-        }
-        return filteredWork;
-    }
-    genererPhotos(filteredWork); 
-
-}
-      
 
 /**********************************************Event Listener**********************************************************/
 // no filter, show all button
@@ -64,22 +50,31 @@ noFilterBtn.addEventListener("click", () => {
 // Filter for Objects
 const objectsFilterBtn = document.querySelector(".objects-btn");
 objectsFilterBtn.addEventListener("click", () => {
-   noFilterBtnClass(noFilterBtn);
-   filterThis(filteredWork, objects);
+    //function for button class 
+    noFilterBtnClass(noFilterBtn);
+    //supression of the photo gallery content for update
+    photoZone.innerHTML = "";
+    //filter the concerned works
+    const filtered = works.filter((x) => x.categoryId === 1);
+    //function to generate gallery anew with filter
+    genererPhotos(filtered);   
 }); 
 
+// Filter for appartements
 const appartementsFilterBtn = document.querySelector(".appart-btn");
 appartementsFilterBtn.addEventListener("click", () => {
     noFilterBtnClass(noFilterBtn); 
-    photoZone.innerHTML = ""; 
-
+    photoZone.innerHTML = "";
+    const filtered = works.filter((x) => x.categoryId === 2);
+    genererPhotos(filtered); 
 });
 
+// Filter for hotels and resto
 const hotelRestoFilterBtn = document.querySelector(".hotelResto-btn"); 
 hotelRestoFilterBtn.addEventListener("click", () => {
-    // btn-class
     noFilterBtnClass(noFilterBtn);
-    photoZone.innerHTML = ""; 
-
+    photoZone.innerHTML = "";
+    const filtered = works.filter((x) => x.categoryId === 3);
+    genererPhotos(filtered); 
 });
 
