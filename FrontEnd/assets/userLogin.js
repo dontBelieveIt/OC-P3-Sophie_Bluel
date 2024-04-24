306a1abbc27123bc3c16be833c25254a2b6cfee1
Roxane <roxane.vidoni@gmail.com>
2024-04-24 11:47:40 +0200

    Step 2.2 done. User authentication done with token.

--- FrontEnd/assets/userLogin.js
+++ FrontEnd/assets/userLogin.js
@@ -1,27 +1,38 @@
-const apiUrl = 'http://localhost:5678/api/users/login';
-const requestBody = {
-"email": "string",
-"password": "string"
+function login() {
+    const loginForm = document.getElementById("form");
+    
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        
+        const user = {
+            email: e.target.querySelector("[name=email]").value,
+            password: e.target.querySelector("[name=password]").value,
+        };
+
+        const formData = JSON.stringify(user);
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: formData
+        })
+
+        .then(response => {
+            if (response.status !== 200) {
+                alert("Erreur : les informations de connexion sont incorrectes. Veuillez réessayer.");
+            } else {
+                return response.json();
+            }
+            
+        })
+        .then(data => {
+            const token = data.token;
+            sessionStorage.setItem("authToken", token);
+            window.location.href = "index.html";
+        })
+        .catch(err => {
+            console.log(err);
+            alert("Une erreur s'est produite lors de la connexion. Veuillez réessayer plus tard.");
+        });
+    });
 }
-// login script
-const loginForm = document.getElementById("form");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const submitInfo = document.getElementById("submit");
-
-
-submitInfo.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-    const userInfo = new FormData(form);
-    console.log(...userInfo);
-    
-   fetch('http://localhost:5678/api/users/login', {
-    method: "POST", 
-    body: userInfo
-   })  
-   .then(res => res.json())
-   .then(data => console.log(data))
-   .catch(err => console.log(err))      
-});
-
+login()
