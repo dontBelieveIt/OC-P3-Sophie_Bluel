786c2c1a12f099974bb12b15467e83194364cdef
Roxane <roxane.vidoni@gmail.com>
2024-04-25 00:32:53 +0200

    Addition of the first draft of the second modal for adding photos, once autheticated.

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,17 +1,24 @@
 // To fetch the ressources in the gallerie-content of the modal
 const response = await fetch('http://localhost:5678/api/works');
 let works = await response.json(); 
-/**********************************************Open and close modal**********************************************************/
+
+// const for first modal
 const openModal = document.querySelector("#data-open-modal");
 const modal = document.querySelector("#data-modal");
 const closeModal = document.querySelector(".btn-close-modal");
 const dialog = document.querySelector("dialog");
+// const for the add photo modal
+const openAddPhotoModal = document.getElementById("openAddModal"); 
+const addPhotoModal = document.getElementById("addPhotoModal");
+const btnReturnMainModal = document.getElementById("return-main-modal");
 // const overlay = document.querySelector(".overlay");
-
+/**********************************************Open and close modal**********************************************************/
 // To open the Modal onclick
 openModal.onclick = function() {
   modal.style.display = "block"; 
   dialog.style.display = "block";
+
+  addPhotoModal.style.display = "none";
   // overlay.style.display = "block"; 
 };
 
@@ -19,6 +26,8 @@ openModal.onclick = function() {
 closeModal.onclick = function() {
   modal.style.display = "none";
   dialog.style.display = "none";
+
+  addPhotoModal.style.display = "none";
   // overlay.style.display = "none";
 };
 
@@ -31,12 +40,28 @@ function genererModalPhotos(works){
         const modalFigure = document.createElement("figure");
         const modalPhotoURL = document.createElement("img");
         modalPhotoURL.src = modalPhotos.imageUrl;
-        
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-btn");
+        deleteButton.innerHTML = `<span class="material-symbols-outlined">
+        delete
+        </span>`
+
         modalWorks.appendChild(modalFigure);
         modalFigure.appendChild(modalPhotoURL); 
+        modalFigure.appendChild(deleteButton);
      }
 }
 genererModalPhotos(works);
+
+/**********************************************Add photo Modal**********************************************************/
+openAddPhotoModal.onclick = function() {
+  modal.style.display = "none"; 
+  addPhotoModal.style.display = "block";
+}
+btnReturnMainModal.onclick = function() {
+  modal.style.display = "block"; 
+  addPhotoModal.style.display = "none";
+}
 
 
 // // Check if the user has a token
