03586f33e0e89d120a57f1b2e12fdc9b35612a77
Roxane <roxane.vidoni@gmail.com>
2024-05-08 20:58:11 +0200

    back to javascript

--- FrontEnd/assets/userLogin.js
+++ FrontEnd/assets/userLogin.js
@@ -4,51 +4,57 @@
 
 *Contents: 
     Function login() : Line 
-    **Submit Form add Event Listener
-
+    ***Submit Form add Event Listener
     login/logout Button : Line 
+    ***edition mode header : Line
 
   For the modals gestions and their functions, go see the userLogin.js file (../assets/modal.js); 
   For the gallery section of the index.html file and the filter button, see the filter.js file (../assets/filter.js); 
  */
-
-
-
 /**********************************************Login Function and Add Event listener**********************************************************/
+//Use the api to search whether the user is registered or not. 
+//*** IF the user is registered, produce a authentication token to access the edition mode 
+//*** if the user is not registered, or if the informations provideed (email/mdp) are not correct, return an error message 
 function login() {
-     
-    // Define first the elements we will need to identify the user
-    const user = {
-        email: document.querySelector("[name=email]").value,
-        password: document.querySelector("[name=password]").value,
-    };
-
-    // Elements to define the user are then sent to the login API
-    const formData = JSON.stringify(user);
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: formData
-    })
-
-    .then(res => {
-        if (res.status !== 200) {
-            alert("Erreur : les informations de connexion sont incorrectes. Veuillez réessayer.");
-        } else {
-            return res.json();
-        }
+    const loginForm = document.querySelector("form");
+    const errorMsgZone = document.getElementById("errorMsg"); 
+    errorMsgZone.style.display = "none"; 
+    
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
         
-    })
-    .then(auth => {
-        const authorization = data.token;
-        sessionStorage.setItem("authenticationToken", token);
-        window.location.href = "index.html";
-    })
-    .catch(err => {
-        console.log(err);
-        alert("Une erreur s'est produite lors de la connexion. Veuillez réessayer plus tard.");
+        const user = {
+            email: e.target.querySelector("[name=email]").value,
+            password: e.target.querySelector("[name=password]").value,
+        };
+
+        const formData = JSON.stringify(user);
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: formData
+        })
+
+        .then(response => {
+            if (response.status !== 200) {
+                errorMsgZone.style.display = "block";
+                errorMsgZone.innerText = "Erreur : les informations de connexion sont incorrectes. Veuillez réessayer.";
+            } else {
+                return response.json();
+            }
+        })
+        .then(authorization => {
+            const token = authorization.token;
+            sessionStorage.setItem("authenticationToken", token);
+            window.location.href = "index.html";
+        })
+        .catch(err => {
+            console.log(err);
+            errorMsgZone.style.display = "block";
+            errorMsgZone.innerText = "Une erreur s'est produite lors de la connexion. Veuillez réessayer plus tard.";
+        });
     });
-};
+}
 
 /*Add Event Listener for the Submit button from the form present  on the login.html file*/
 const loginForm = document.querySelector("form");
@@ -56,8 +62,6 @@ loginForm.addEventListener("submit", function (e) {
     e.preventDefault();
     login();
 });
-
-
 
 /**********************************************login/logout button**********************************************************/
 // This function takes into account wheter the user is successfully logged in or not. 
@@ -67,8 +71,13 @@ loginForm.addEventListener("submit", function (e) {
 // ***editing works on the index.html file will be impossible. 
 
 function logged() {
+    //For the Login/Log out button, depending whether the user has been successfully logged in 
+    //***if the logged in is successful, then the log out option will be presented, and it will erase the token */
     const logoutBtn = document.querySelector('.logOut');
     const loginBtn = document.querySelector('.logIn');
+
+    //If the user is successfully logged in, a top banner will be displayed on which can be read "Edition Mode"
+    const editModeBanner = document.querySelector(".edit-mode"); 
 
     const token = sessionStorage.getItem("authenticationToken");
     console.log(token);
@@ -76,9 +85,13 @@ function logged() {
     if (token) {
         logoutBtn.style.display = "block"; 
         loginBtn.style.display = "none"; 
+
+        editModeBanner.style.display = "flex"; 
     } else {
         logoutBtn.style.display = "none"; 
         loginBtn.style.display = "block"; 
+
+        editModeBanner.style.display = "none";
     };
 
     // Once the token has been verified as present and the logout button has appeared, it's possible for the user to log out by clicking on this button. 
